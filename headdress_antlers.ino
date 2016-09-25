99187f7dd6fa2a7f1148cb35b590df9529c3ac3f
Tom Shea <tomjonshea@gmail.com>
2016-09-25 20:41:17 +0100

    remove unused code and meet standards

--- headdress_antlers.ino
+++ headdress_antlers.ino
@@ -1,364 +1,156 @@
 #include "FastLED.h"
 
-FASTLED_USING_NAMESPACE
-
-// FastLED "100-lines-of-code" demo reel, showing just a few
-// of the kinds of animation patterns you can quickly and easily
-// compose using FastLED.
-//
-// This example also shows one easy way to define multiple
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
-
-#if FASTLED_VERSION < 3001000
-#error "Requires FastLED 3.1 or later; check github for latest code."
-#endif
-
-#define DATA_PIN    6
-//#define CLK_PIN   4
+// Hardware definitions
+#define DATA_PIN    1
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
-#define NUM_LEDS    2700
+#define NUM_LEDS    66
+
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS          255
+// Software definitions
+#define BRIGHTNESS          96
 #define FRAMES_PER_SECOND  60
+#define HUE_CHANGE_MILLISECONDS 20
+#define PATTERN_CHANGE_SECONDS 60
 
 //For fire
 #define COOLING  55
 #define SPARKING 120
 
-//For noise
-const uint8_t kMatrixWidth  = 11;
-const uint8_t kMatrixHeight = 6;
-#define MAX_DIMENSION ((kMatrixWidth>kMatrixHeight) ? kMatrixWidth : kMatrixHeight)
-static uint16_t x;
-static uint16_t y;
-static uint16_t z;
-uint16_t speed = 20;
-uint16_t scale = 30;
-uint8_t noise[MAX_DIMENSION][MAX_DIMENSION];
-CRGBPalette16 currentPalette( PartyColors_p );
+CRGBPalette16 currentPalette(PartyColors_p);
 uint8_t       colorLoop = 1;
 
 void setup() {
-  //delay(3000); // 3 second delay for recovery
+    delay(1000); // 1 second delay for recovery
 
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
-  // set master brightness control
-  FastLED.setBrightness(BRIGHTNESS);
-
-  //For noise
-  x = random16();
-  y = random16();
-  z = random16();
+    FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(BRIGHTNESS);
 }
-
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { sinelon, Fire2012, sinelon, rainbow, rainbowWithGlitter, cylon, confetti, sinelon, juggle, sinelon, bpm };
+SimplePatternList gPatterns = {sinelon, Fire2012, sinelon, rainbow, rainbowWithGlitter, glitterOnlyWhite, glitterOnlyRainbow, sinelon, confetti, sinelon, juggle, sinelon, bpm};
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 void loop()
 {
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
+    // Call the current pattern function once, updating the 'leds' array
+    gPatterns[gCurrentPatternNumber]();
 
-  // send the 'leds' array out to the actual LED strip
-  FastLED.show();
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND);
+    // send the 'leds' array out to the actual LED strip
+    FastLED.show();
+    // insert a delay to keep the framerate modest
+    FastLED.delay(1000/FRAMES_PER_SECOND);
 
-  // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 60 ) { nextPattern(); } // change patterns periodically
+    // do some periodic updates
+    EVERY_N_MILLISECONDS(HUE_CHANGE_MILLISECONDS) { gHue++; } // slowly cycle the "base color" through the rainbow
+    EVERY_N_SECONDS(PATTERN_CHANGE_SECONDS) {nextPattern();} // change patterns periodically
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
 void nextPattern()
 {
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+    // add one to the current pattern number, and wrap around at the end
+    gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
 }
 
 void rainbow()
 {
-  // FastLED's built-in rainbow generator
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+    // FastLED's built-in rainbow generator
+    fill_rainbow(leds, NUM_LEDS, gHue, 7);
 }
 
 void rainbowWithGlitter()
 {
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(80);
+    // built-in FastLED rainbow, plus some random sparkly glitter
+    rainbow();
+    addGlitter(80);
 }
 
-void addGlitter( fract8 chanceOfGlitter)
+void addGlitter(fract8 chanceOfGlitter)
 {
-  if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
-  }
+    if(random8() < chanceOfGlitter) {
+        leds[random16(NUM_LEDS)] += CRGB::White;
+    }
+}
+
+void glitterOnlyWhite() {
+    leds[random16(NUM_LEDS)] += CRGB::White
+    fadeToBlackBy(leds, NUM_LEDS, 50);
+    delay(15);
+}
+
+void glitterOnlyRainbow() {
+    CRGB onePix[1];
+    fill_rainbow (onePix, 1, gHue, 7);
+    leds[random16(NUM_LEDS)] = onePix[0];
+    fadeToBlackBy(leds, NUM_LEDS, 50);
+    delay(15);
 }
 
 void confetti()
 {
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+    // random colored speckles that blink in and fade smoothly
+    fadeToBlackBy(leds, NUM_LEDS, 10);
+    int pos = random16(NUM_LEDS);
+    leds[pos] += CHSV(gHue + random8(64), 200, 255);
 }
 
 void sinelon()
 {
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16(13,0,NUM_LEDS);
-  leds[pos] += CHSV( gHue, 255, 192);
+    // a colored dot sweeping back and forth, with fading trails
+    fadeToBlackBy(leds, NUM_LEDS, 20);
+    int pos = beatsin16(13, 0, NUM_LEDS);
+    leds[pos] += CHSV(gHue, 255, 192);
 }
 
 void bpm()
 {
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_LEDS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
-  }
+    // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+    uint8_t beatsPerMinute = 62;
+    uint8_t beat = beatsin8(beatsPerMinute, 64, 255);
+    for (uint8_t i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette(currentPalette, gHue + (i * 2), beat - gHue + (i * 10));
+    }
 }
 
 void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16(i+7,0,NUM_LEDS)] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
+    // eight colored dots, weaving in and out of sync with each other
+    fadeToBlackBy(leds, NUM_LEDS, 20);
+    byte dothue = 0;
+    for(uint8_t i = 0; i < 8; i++) {
+        leds[beatsin16(i + 7, 0, NUM_LEDS)] |= CHSV(dothue, 200, 255);
+        dothue += 32;
+    }
 }
 
 void Fire2012()
 {
-// Array of temperature readings at each simulation cell
-  static byte heat[NUM_LEDS];
+    // Array of temperature readings at each simulation cell
+    static byte heat[NUM_LEDS];
 
-  // Step 1.  Cool down every cell a little
-    for( int i = 0; i < NUM_LEDS; i++) {
-      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    // Step 1.  Cool down every cell a little
+    for (uint8_t i = 0; i < NUM_LEDS; i++) {
+        heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
     }
 
     // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int k= NUM_LEDS - 1; k >= 2; k--) {
-      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    for (uint8_t k = NUM_LEDS - 1; k >= 2; k--) {
+        heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
     }
 
     // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-    if( random8() < SPARKING ) {
-      int yFire = random8(7);
-      heat[yFire] = qadd8( heat[yFire], random8(160,255) );
+    if (random8() < SPARKING) {
+        int yFire = random8(7);
+        heat[yFire] = qadd8(heat[yFire], random8(160, 255));
     }
 
     // Step 4.  Map from heat cells to LED colors
-    for( int j = 0; j < NUM_LEDS; j++) {
-        leds[j] = HeatColor( heat[j]);
+    for (int j = 0; j < NUM_LEDS; j++) {
+        leds[j] = HeatColor(heat[j]);
     }
-}
-
-void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
-
-void cylon() {
-  static uint8_t hue = 0;
-	Serial.print("x");
-	// First slide the led in one direction
-	for(int i = 0; i < NUM_LEDS; i++) {
-		// Set the i'th led to red
-		leds[i] = CHSV(hue++, 255, 255);
-		// Show the leds
-		FastLED.show();
-		// now that we've shown the leds, reset the i'th led to black
-		// leds[i] = CRGB::Black;
-		fadeall();
-		// Wait a little bit before we loop around and do it again
-		delay(10);
-	}
-	Serial.print("x");
-
-	// Now go in the other direction.
-	for(int i = (NUM_LEDS)-1; i >= 0; i--) {
-		// Set the i'th led to red
-		leds[i] = CHSV(hue++, 255, 255);
-		// Show the leds
-		FastLED.show();
-		// now that we've shown the leds, reset the i'th led to black
-		// leds[i] = CRGB::Black;
-		fadeall();
-		// Wait a little bit before we loop around and do it again
-		delay(10);
-	}
-}
-
-// Fill the x/y array of 8-bit noise values using the inoise8 function.
-void fillnoise8() {
-  // If we're runing at a low "speed", some 8-bit artifacts become visible
-  // from frame-to-frame.  In order to reduce this, we can do some fast data-smoothing.
-  // The amount of data smoothing we're doing depends on "speed".
-  uint8_t dataSmoothing = 0;
-  if( speed < 50) {
-    dataSmoothing = 200 - (speed * 4);
-  }
-
-  for(int i = 0; i < MAX_DIMENSION; i++) {
-    int ioffset = scale * i;
-    for(int j = 0; j < MAX_DIMENSION; j++) {
-      int joffset = scale * j;
-
-      uint8_t data = inoise8(x + ioffset,y + joffset,z);
-
-      // The range of the inoise8 function is roughly 16-238.
-      // These two operations expand those values out to roughly 0..255
-      // You can comment them out if you want the raw noise data.
-      data = qsub8(data,16);
-      data = qadd8(data,scale8(data,39));
-
-      if( dataSmoothing ) {
-        uint8_t olddata = noise[i][j];
-        uint8_t newdata = scale8( olddata, dataSmoothing) + scale8( data, 256 - dataSmoothing);
-        data = newdata;
-      }
-
-      noise[i][j] = data;
-    }
-  }
-
-  z += speed;
-
-  // apply slow drift to X and Y, just for visual variation.
-  x += speed / 8;
-  y -= speed / 16;
-}
-
-void mapNoiseToLEDsUsingPalette()
-{
-  static uint8_t ihue=0;
-
-  for(int i = 0; i < kMatrixWidth; i++) {
-    for(int j = 0; j < kMatrixHeight; j++) {
-      // We use the value at the (i,j) coordinate in the noise
-      // array for our brightness, and the flipped value from (j,i)
-      // for our pixel's index into the color palette.
-
-      uint8_t index = noise[j][i];
-      uint8_t bri =   noise[i][j];
-
-      // if this palette is a 'loop', add a slowly-changing base value
-      if( colorLoop) {
-        index += ihue;
-      }
-
-      // brighten up, as the color palette itself often contains the
-      // light/dark dynamic range desired
-      if( bri > 127 ) {
-        bri = 255;
-      } else {
-        bri = dim8_raw( bri * 2);
-      }
-
-      CRGB color = ColorFromPalette( currentPalette, index, bri);
-      leds[i + j * kMatrixWidth] = color;
-    }
-  }
-
-  ihue+=1;
-}
-
-#define HOLD_PALETTES_X_TIMES_AS_LONG 1
-
-void ChangePaletteAndSettingsPeriodically()
-{
-  uint8_t secondHand = ((millis() / 1000) / HOLD_PALETTES_X_TIMES_AS_LONG) % 60;
-  static uint8_t lastSecond = 99;
-
-  if( lastSecond != secondHand) {
-    lastSecond = secondHand;
-    if( secondHand ==  0)  { currentPalette = RainbowColors_p;         speed = 20; scale = 30; colorLoop = 1; }
-    if( secondHand ==  5)  { SetupPurpleAndGreenPalette();             speed = 10; scale = 50; colorLoop = 1; }
-    if( secondHand == 10)  { SetupBlackAndWhiteStripedPalette();       speed = 20; scale = 30; colorLoop = 1; }
-    if( secondHand == 15)  { currentPalette = ForestColors_p;          speed =  8; scale =120; colorLoop = 0; }
-    if( secondHand == 20)  { currentPalette = CloudColors_p;           speed =  4; scale = 30; colorLoop = 0; }
-    if( secondHand == 25)  { currentPalette = LavaColors_p;            speed =  8; scale = 50; colorLoop = 0; }
-    if( secondHand == 30)  { currentPalette = OceanColors_p;           speed = 20; scale = 90; colorLoop = 0; }
-    if( secondHand == 35)  { currentPalette = PartyColors_p;           speed = 20; scale = 30; colorLoop = 1; }
-    if( secondHand == 40)  { SetupRandomPalette();                     speed = 20; scale = 20; colorLoop = 1; }
-    if( secondHand == 45)  { SetupRandomPalette();                     speed = 50; scale = 50; colorLoop = 1; }
-    if( secondHand == 50)  { SetupRandomPalette();                     speed = 90; scale = 90; colorLoop = 1; }
-    if( secondHand == 55)  { currentPalette = RainbowStripeColors_p;   speed = 30; scale = 20; colorLoop = 1; }
-  }
-}
-
-// This function generates a random palette that's a gradient
-// between four different colors.  The first is a dim hue, the second is
-// a bright hue, the third is a bright pastel, and the last is
-// another bright hue.  This gives some visual bright/dark variation
-// which is more interesting than just a gradient of different hues.
-void SetupRandomPalette()
-{
-  currentPalette = CRGBPalette16(
-                      CHSV( random8(), 255, 32),
-                      CHSV( random8(), 255, 255),
-                      CHSV( random8(), 128, 255),
-                      CHSV( random8(), 255, 255));
-}
-
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-void SetupBlackAndWhiteStripedPalette()
-{
-  // 'black out' all 16 palette entries...
-  fill_solid( currentPalette, 16, CRGB::Black);
-  // and set every fourth one to white.
-  currentPalette[0] = CRGB::White;
-  currentPalette[4] = CRGB::White;
-  currentPalette[8] = CRGB::White;
-  currentPalette[12] = CRGB::White;
-
-}
-
-// This function sets up a palette of purple and green stripes.
-void SetupPurpleAndGreenPalette()
-{
-  CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-  CRGB green  = CHSV( HUE_GREEN, 255, 255);
-  CRGB black  = CRGB::Black;
-
-  currentPalette = CRGBPalette16(
-    green,  green,  black,  black,
-    purple, purple, black,  black,
-    green,  green,  black,  black,
-    purple, purple, black,  black );
-}
-
-void noiseLoop() {
-    // Periodically choose a new palette, speed, and scale
-  ChangePaletteAndSettingsPeriodically();
-
-  // generate noise data
-  fillnoise8();
-
-  // convert the noise data to colors in the LED array
-  // using the current palette
-  mapNoiseToLEDsUsingPalette();
-
-  LEDS.show();
-  //delay(20);
 }
